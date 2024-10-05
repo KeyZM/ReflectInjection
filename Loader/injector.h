329ee070c7c07d5b6f5651b153a56759cfbb6f13
KeyZM <mnbvcxz79513@163.com>
2024-10-05 10:32:12 +0800

    1005
    1005

--- Loader/injector.h
+++ Loader/injector.h
@@ -1,6 +1,18 @@
 #pragma once
 
 #include <Windows.h>
+#include "Interface.hpp"
+
+#define RELOC_FLAG32(RelInfo) ((RelInfo >> 0x0C) == IMAGE_REL_BASED_HIGHLOW)
+#define RELOC_FLAG64(RelInfo) ((RelInfo >> 0x0C) == IMAGE_REL_BASED_DIR64)
+
+#ifdef _WIN64
+#define RELOC_FLAG RELOC_FLAG64
+#define CURRENT_ARCH IMAGE_FILE_MACHINE_AMD64
+#else
+#define RELOC_FLAG RELOC_FLAG32
+#define CURRENT_ARCH IMAGE_FILE_MACHINE_I386
+#endif
 
 using fn_LoadLibraryA = HINSTANCE(WINAPI*)(const char* lpLibFilename);
 using fn_GetProcAddress = FARPROC(WINAPI*)(HMODULE hModule, LPCSTR lpProcName);
