b5a16c236791515429682571fc74453a02315473
KeyZM <mnbvcxz79513@163.com>
2024-10-12 21:14:40 +0800

    忘了
    忘了

--- Loader/Interface.hpp
+++ Loader/Interface.hpp
@@ -1,1 +1,29 @@
 #pragma once
+
+#include "Pch.h"
+
+class Interfaces {
+
+public:
+
+	virtual bool Initialize(DWORD pid) = 0;
+
+	virtual ~Interfaces() = 0 {};
+
+
+	// 开辟/释放内存
+	virtual void* MmAlloc(size_t size) = 0;
+
+	virtual bool MmFree(void* addr) = 0;
+
+	virtual bool MmProtect(void* addr, DWORD NewProtect, DWORD64 Size) { return false; };
+
+	// 读/写内存
+	virtual bool MmRead(void* addr, void* buffer, size_t size) = 0;
+
+	virtual bool MmWrite(void* addr, void* data, size_t size) = 0;
+
+
+	virtual bool CallShellCode(void* pShellCode, void* param) = 0;
+
+};
--- Loader/Main.cpp
+++ Loader/Main.cpp
@@ -11,4 +11,4 @@ int main() {
 		//inject
 	}
 	return 0;
-}
+}	
